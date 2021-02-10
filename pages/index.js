78b54a002a634898476dc39d343d80e73956af96
Milena <rodriguesmilena.si@gmail.com>
2021-02-10 17:05:59 -0300

    Criação do projeto instaAlura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+function Title(props){
+  return <h1>Titulooo.. {props.titulo}</h1>
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <div><Title titulo="Enviando props" /></div>
 }
