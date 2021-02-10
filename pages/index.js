9c4a2822e21f15e679e4baada31b17e53852ab36
Milena <rodriguesmilena.si@gmail.com>
2021-02-10 18:45:36 -0300

    Criação do componente MENU e estilização

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import styled from 'styled-components'
-
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
-
-function Title(props){
-  return <h1>Titulooo.. {props.titulo}</h1>
-}
+import Menu from '../src/components/commons/Menu'
 
 export default function Home() {
-  return <div><Title titulo="Enviando props" /></div>
+  return (
+    <div>
+      <Menu />
+    </div>
+  )
 }
